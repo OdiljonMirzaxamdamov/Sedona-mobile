63f56a39ed322d86b016965b8b20846c3f270fba
OdiljonMirzaxamdamov <odiojon1994@gmail.com>
2023-11-04 20:43:18 +0500

    Подключил Ретину и  JS для форм/отзыв

--- src/js/main-nav-js.js
+++ src/js/main-nav-js.js
@@ -1,13 +1,7 @@
 
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
-// var link = document.querySelector('.main-nav__user-login');
-// var popup = document.querySelector(".modal-login");
-// var close = popup.querySelector(".modal-login__close");
-// var form = popup.querySelector("form");
-// var login = popup.querySelector("[name=login]");
-// var password = popup.querySelector("[name=password]");
-// var storage = localStorage.getItem("login")
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
 
     navMain.classList.remove('main-nav--nojs');
 
@@ -21,13 +15,3 @@ var navToggle = document.querySelector('.main-nav__toggle');
         }
     });
 
-
-//     link.addEventListener("click", function(event) {
-//     event.preventDefault();
-//     popup.classList.add("modal-login--show");
-// });
-//
-//     close.addEventListener("click", function(event) {
-//     event.preventDefault();
-//     popup.classList.remove("modal-login--show");
-// });
---
+++ src/js/modal-form.js
@@ -0,0 +1,40 @@
+const formFeedbackButton = document.querySelector(".form-feedback__button");
+const modalFormSuccess = document.querySelector(".modal-form--success");
+const modalFormFailure = document.querySelector(".modal-form--failure");
+const modalFormButtonSuccess = document.querySelector(".modal-form__button--success");
+const modalFormButtonFailure = document.querySelector(".modal-form__button--failure");
+
+const form = document.querySelector("form");
+const firstname = form.querySelector("[name=firstname]");
+const lastname = form.querySelector("[name=lastname]");
+const phone = form.querySelector("[name=phone]");
+const email = form.querySelector("[name=email]");
+
+
+formFeedbackButton.addEventListener("click", function (evt){
+    evt.preventDefault();
+    if (!firstname.value || !lastname.value || !phone.value || !email.value) {
+        modalFormFailure.classList.remove("modal-form--remove");
+    } else {
+        modalFormSuccess.classList.remove("modal-form--remove");
+    }
+});
+
+modalFormButtonSuccess.addEventListener("click", function (evt){
+    evt.preventDefault();
+    modalFormSuccess.classList.add("modal-form--remove");
+});
+
+modalFormButtonFailure.addEventListener("click", function (evt){
+    evt.preventDefault();
+    modalFormFailure.classList.add("modal-form--remove");
+});
+
+
+window.addEventListener("keydown", function (evt){
+    if (evt.keyCode === 27) {
+        evt.preventDefault();
+        modalFormSuccess.classList.add("modal-form--remove")
+        modalFormFailure.classList.add("modal-form--remove");
+    }
+});
